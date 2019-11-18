70f6ffd4c2a1ecf4a1c676cf7d5513b1e74ad008
Petr Molokotin <molokotinpa@msk.bcs.ru>
2019-11-18 12:56:54 +0300

    added swagger-ui

--- src/main/java/ru/bcs/requestrecorder/config/ClientController.java
+++ src/main/java/ru/bcs/requestrecorder/config/ClientController.java
@@ -1,5 +1,6 @@
 package ru.bcs.requestrecorder.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.bcs.at.library.core.core.helpers.MitmInterceptedMessage;
 import ru.bcs.requestrecorder.MessageCache;
 
+@Api(tags = { "client" })
 @RestController
 public class ClientController {
 
---
+++ src/main/java/ru/bcs/requestrecorder/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package ru.bcs.requestrecorder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ru.bcs.requestrecorder"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("client", "API for client."));
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Mitmproxy Recorder API").version("0.2").build();
+    }
+
+}
